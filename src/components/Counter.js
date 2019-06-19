d446c18fe1b03630d884989894da7291ea528ac5
schang1146 <sammychang2185@gmail.com>
2019-06-19 16:18:18 -0700

    basic counter works

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -17,13 +17,14 @@ class Counter extends Component {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
+        console.log(this)
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { this.props.increment(this.props.count) }}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { this.props.decrement(this.props.count) }}>
                     -
                 </button>
                 {/* Uncomment these button tags if you got
